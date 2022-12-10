4a4a3a9ee4f8c97dc4da3014ab5835d1392e2313
Zeyad-Diaa12 <100387591+Zeyad-Diaa12@users.noreply.github.com>
2022-12-10 23:35:05 +0200

    Add files via upload

--- src/bugtrackingsystem/BugTrackingSystem.java
+++ src/bugtrackingsystem/BugTrackingSystem.java
@@ -2,13 +2,12 @@ package bugtrackingsystem;
 
 import bugtrackingsystem.forms.Login1;
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class BugTrackingSystem {
 
     public static void main(String[] args) throws SQLException {
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new Login1().setVisible(true);
             }
